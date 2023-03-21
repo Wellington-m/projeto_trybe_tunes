920e600ac376c5a48dda673bd24cd97c0cdee60a
Wellington <wellingtonmachioni@gmail.com>
2023-03-21 17:12:47 -0300

    refactor: estilizado pagina de login e aplicado responsividade

--- src/App.js
+++ src/App.js
@@ -3,10 +3,37 @@ import { createGlobalStyle } from 'styled-components';
 import Routes from './components/Routes';
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    padding: 0px;
-    font-family: sans-serif;
-    margin: 0px;
+html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+  
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+  
+  ol, ul {
+    list-style: none;
+  }
+  
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  :root {
+    --main-color: #FAFAFA;
+    --secondary-color: #121725;
+    --accent: #003BE5;
+    --dark-green: #023031;
+    --light-green: #036B52;
+    --white: white;
   }
 `;
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import FormInput from '../components/FormInput';
 import { createUser } from '../services/userAPI';
 import Loading from '../components/Loading';
+import logo from '../images/logo.svg';
+
+const PageLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  img {
+    margin-top: 169px;
+    margin-bottom: 104px;
+  }
+
+  @media (max-width: 599px) {
+    margin-top: 50px;
+    img {
+      margin: 0;
+      margin-bottom: 50px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    img {
+      margin-top: 0;
+    }
+  }
+
+  @media (min-width: 1800px) {
+    background: grey;
+  }
+`;
+
+const FormLogin = styled.form`
+  background-color: var(--white);
+  border-radius: 10px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  height: 314px;
+  padding: 94px;
+  width: 697px;
+  input {
+    border: 1px solid #E1E5EB;
+    outline: none;
+    padding: 12px 16px;
+    width: 100%;
+  }
+  button {
+    background-color: var(--accent);
+    border: none;
+    border-radius: 2px;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+    color: var(--white);
+    cursor: pointer;
+    margin-top: 31px;
+    padding: 8px 32px;
+    width: 100%;
+  }
+
+  @media (max-width: 768px) {
+    max-width: 90%;
+    padding-left: 24px;
+    padding-right: 24px;
+  }
+`;
 
 class Login extends React.Component {
   constructor() {
@@ -34,8 +98,9 @@ class Login extends React.Component {
     }
 
     return (
-      <div data-testid="page-login">
-        <form onSubmit={ this.handleSubmit }>
+      <PageLogin data-testid="page-login">
+        <img src={ logo } alt="logo da Trybe" />
+        <FormLogin onSubmit={ this.handleSubmit }>
           <FormInput
             htmlFor="name"
             placeHolder="Nome"
@@ -54,8 +119,8 @@ class Login extends React.Component {
             Entrar
           </button>
 
-        </form>
-      </div>);
+        </FormLogin>
+      </PageLogin>);
   }
 }
 
