61939ce3b81339039c0edd1ce07774181d2770b0
Wellington <wellingtonmachioni@gmail.com>
2023-03-24 16:23:29 -0300

    fix: corrigido pagina de favoritos

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import MusicCard from '../components/MusicCard';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
   constructor() {
@@ -27,22 +27,32 @@ class Favorites extends React.Component {
     this.setState({ isLoading: false });
   }
 
+  removeFavorite = async (musicId) => {
+    this.setState({
+      isLoading: true,
+    });
+    const { allFavoriteSongs } = this.state;
+    await removeSong(allFavoriteSongs.find((s) => s.trackId === musicId));
+    this.getFavorite();
+  }
+
   render() {
     const { allFavoriteSongs, isLoading } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
-        { isLoading ? <Loading /> : allFavoriteSongs.map((music) => (
-          <MusicCard
-            key={ music.trackId }
-            trackId={ music.trackId }
-            trackName={ music.trackName }
-            previewUrl={ music.previewUrl }
-            favoriteSongs={ allFavoriteSongs }
-            music={ music }
-            attFavorites={ this.getFavorite }
-          />
-        ))}
+        { isLoading
+          ? <Loading loadMessage="favoritos" />
+          : allFavoriteSongs.map((music) => (
+            <MusicCard
+              key={ music.trackId }
+              trackName={ music.trackName }
+              previewUrl={ music.previewUrl }
+              trackId={ music.trackId }
+              checked
+              attFavorites={ () => this.removeFavorite(music.trackId) }
+            />
+          ))}
       </div>
     );
   }
