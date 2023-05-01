caf9852583b1bc9fe1e0c2abb0c8b75b35a758ee
Wellington <wellingtonmachioni@gmail.com>
2023-05-01 17:46:06 -0300

    refactor: refatorado loading e MusicCard para styledComponents

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import './Loading.css';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    background: #fafafa;
+    height: 100%;
+    left: 1px;
+    position: absolute;
+    top: 1px;
+    width: 100%;
+    z-index: 1;
+`;
+
+const P = styled.p`
+    color: #023031;
+    font-family: Epilogue , Helvetica , Arial , Verdana , Tahoma , sans-serif;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 300;
+    letter-spacing: 0.02em;
+    line-height: 65px;
+    position: absolute;
+`;
 
 class Loading extends React.Component {
   render() {
     return (
-      <div className="loading">
-        <p>
+      <Div className="loading">
+        <P>
           Carregando...
-        </p>
-      </div>
+        </P>
+      </Div>
     );
   }
 }
--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import './MusicCard.css';
+
+const HeartBoxSpan = styled.span`
+    display: inline-block;
+    height: 100px;
+    position: relative;
+    width: 100px;
+`;
+
+const HeartSpan = styled.span`
+    background: url('http://i.imgur.com/zw8ahUb.png') no-repeat 0 0;
+    display: inline-block;
+    height: inherit;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: inherit;
+`;
+
+const MusicCardStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  height: 100%;
+  width: 100%;
+`;
 
 class MusicCard extends React.Component {
   render() {
     const { trackName, previewUrl, trackId, checked, attFavorites } = this.props;
     const track = (
       <label htmlFor={ `checkbox-music-${trackId}` }>
-        <span className="tw-heart-box">
+        <HeartBoxSpan className="tw-heart-box">
           <input
             id={ `checkbox-music-${trackId}` }
             type="checkbox"
@@ -16,11 +42,11 @@ class MusicCard extends React.Component {
             onChange={ (e) => attFavorites(e, trackId) }
             data-testid={ `checkbox-music-${trackId}` }
           />
-          <span className="tw-heart" />
-        </span>
+          <HeartSpan className="tw-heart" />
+        </HeartBoxSpan>
       </label>);
     return (
-      <div className="musicCard">
+      <MusicCardStyled>
         <p>{ trackName }</p>
         <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
@@ -30,7 +56,7 @@ class MusicCard extends React.Component {
         </audio>
         { track }
 
-      </div>
+      </MusicCardStyled>
 
     );
   }
